422940c6dea3e711937b1dda758c3b748aa54631
Serj <serj.korneev@gmail.com>
2017-06-07 22:22:55 +0300

    render friends & search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-
+/**
+ * VK_API
+ * @return {Promise}
+ */
 function vkInit() {
     return new Promise((resolve, reject) => {
         VK.init({
@@ -31,8 +34,98 @@ function vkApi(method, options) {
     });
 }
 
+/**
+ * отрисовка элементов
+ * @param {array} friends - массив элементов для прорисовки
+ * @param {} container - элемент DOM в который отрисовывать данные
+ */
+function showFriends(friends, container) {
+    let htmlFragment = new DocumentFragment;
+
+    friends.forEach(friend => {
+        let template = document.createElement('div');
+
+        template.innerHTML = `
+                     <div class="friend" data-id="${friend.id}">
+                         <div class="friend__avatar">
+                            <img src="${friend.photo_100}" class="friend__img">
+                         </div>
+                         <a href="https://vk.com/id${friend.id}" class="friend__name" target="_blank">
+                            ${friend.first_name} ${friend.last_name}
+                         </a>
+                         <button class="friend__btn" type="button">+</button>
+                     </div>
+                  `;
+        htmlFragment.appendChild(template);
+    });
+
+    container.appendChild(htmlFragment);
+}
+
+/**
+ * фильтрация друзей по строке поиска
+ * @param {array} array - массив друзей в котором производим поиск
+ * @param {string} filter - строка по которой фильтруем друзей
+ * @return {array} массив отфильтрованных друзей
+ */
+function filterFriends (array, filter) {
+    let filteredUsers;
+
+    if (filter !== '') {
+        filteredUsers = array.filter( user => {
+            if (user.first_name.toLowerCase().indexOf(filter) !== -1 ||
+                user.last_name.toLowerCase().indexOf(filter) !== -1) {
+                return user;
+            }
+        })
+    } else {
+        filteredUsers = users;
+    }
+
+    return filteredUsers;
+}
+
+let users = [];
+let selectedUsers = [];
+let friendsContainer = document.querySelector('.all-friends .friends-container');
+let selectedContainer = document.querySelector('.all-friends .selected-friends');
+
+// авторизация и загрузка списка друзей ()
+// TODO - сделать авторизацию по клику на кнопке
 window.onload = function () {
     vkInit()
         .then( () => vkApi('friends.get', {fields: 'photo_100, city'}) )
-        .then( response => console.log(response))
+        .then( response => {
+            users = response.items;
+            showFriends(users, friendsContainer);
+        });
 };
+
+// поиск
+// TODO - сделать делегированием на обе колонки
+search.addEventListener('keyup', function (e) {
+    let filter = this.value.toLowerCase();
+    let container = this.parentNode.nextElementSibling.children[1];
+
+    console.log(container);
+    container.innerHTML = '';
+    showFriends(filterFriends(users, filter), container);
+});
+
+// перенос элемента по клику на кнопке
+friendsContainer.addEventListener('click', function (e) {
+    let target = e.target;
+    console.log(target);
+
+    if (target.tagName === 'BUTTON') {
+        let friendID = e.target.parentNode.dataset.id;
+        let ndx = users.findIndex( item => +item.id === +friendID);
+
+        selectedUsers = selectedUsers.concat(users.splice(ndx, 1));
+
+        console.log(selectedUsers);
+        console.log(users)
+
+        showFriends(selectedUsers);
+    }
+});
